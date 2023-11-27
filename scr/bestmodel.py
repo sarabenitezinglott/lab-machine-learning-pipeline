6d39069e2bfd354d180c693e1046756a432f7014
Sara <saraab275@gmail.com>
2023-11-27 11:20:08 +0100

    new changes

--- scr/bestmodel.py
+++ scr/bestmodel.py
@@ -1,37 +1,45 @@
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import ParameterGrid
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.model_selection import ParameterGrid
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn import metrics
-from tqdm import tqdm_notebook
 import numpy as np
 
-def best_model_training(X_train, y_train, X_test, y_test, model, param_grid):
-    rf = model()
-    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid,
-                               scoring='neg_mean_squared_error', cv=3, n_jobs=-1)
+def gardient_training(X_train, y_train, X_test, y_test):
+    #Hyperparameter tuning
+    gradient_param = {
+        'max_depth': [10, 15, 20], 
+        'n_estimators': [100, 200, 500]}
 
-    # Create a progress bar
-    with tqdm_notebook(total=len(list(ParameterGrid(param_grid))), desc="Grid Search Progress") as bar:
-        def update_progress(_):
-            bar.update()
+    gradientgrid = GridSearchCV(GradientBoostingRegressor(random_state=79), gradient_param)
+    gradientgrid.fit(X_train, y_train)
+    print(gradientgrid.best_params_)
+    print('Best score: ', gradientgrid.best_score_)
 
-        # Override the progress_callback to update the progress bar
-        grid_search.progress_callback = update_progress
+    gb_model = GradientBoostingRegressor(max_depth= 10, n_estimators= 100)
+    gb_model.fit(X_train, y_train)
+    y_pred_gb = gb_model.predict(X_test)
+    print(f"MAE, error: {metrics.mean_absolute_error(y_test, y_pred_gb)}")
+    print(f"MSE, error: {metrics.mean_squared_error(y_test, y_pred_gb)}")
+    print(f"RMSE, error: {np.sqrt(metrics.mean_squared_error(y_test, y_pred_gb))}")
+    print(f"r2: {metrics.r2_score(y_test, y_pred_gb)}")
 
-        grid_search.fit(X_train, y_train)
 
-    # Best parameters
-    best_params = grid_search.best_params_
-    print("Best Parameters:", best_params)
-    best_model = grid_search.best_estimator_
-    y_pred = best_model.predict(X_test)
-    print("------------Model Assessment------------\n")
-    print(f"MAE, error: {metrics.mean_absolute_error(y_test, y_pred)}")
-    print(f"MSE, error: {metrics.mean_squared_error(y_test, y_pred)}")
-    print(f"RMSE, error: {np.sqrt(metrics.mean_squared_error(y_test, y_pred))}")
-    print(f"r2: {metrics.r2_score(y_test, y_pred)}")
-    print("\n")
+def forest_training(X_train, y_train, X_test, y_test):
+    #Hyperparameter tuning
+    randforest_param = {
+        'max_depth': [10, 15, 20], 
+        'n_estimators': [100, 200, 500]}
 
-    return best_model
+    forestgrid = GridSearchCV(RandomForestRegressor(random_state=79), randforest_param)
+    forestgrid.fit(X_train, y_train)
+    print(forestgrid.best_params_)
+    print('Best score: ', forestgrid.best_score_)
+
+    rf_model = RandomForestRegressor(max_depth= 15, n_estimators= 500)
+    rf_model.fit(X_train, y_train)
+    y_pred_rf = rf_model.predict(X_test)
+    print(f"MAE, error: {metrics.mean_absolute_error(y_test, y_pred_rf)}")
+    print(f"MSE, error: {metrics.mean_squared_error(y_test, y_pred_rf)}")
+    print(f"RMSE, error: {np.sqrt(metrics.mean_squared_error(y_test, y_pred_rf))}")
+    print(f"r2: {metrics.r2_score(y_test, y_pred_rf)}")
